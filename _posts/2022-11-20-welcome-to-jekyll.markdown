1bd91d1e6d42c249ea1b1387a8912ab832ad4b86
sechenbling <2040095448@qq.com>
2022-11-20 17:33:29 +0800

    pages theme init

---
+++ _posts/2022-11-20-welcome-to-jekyll.markdown
@@ -0,0 +1,8 @@
+---
+layout: post
+title:  "Lynn sites init!"
+date:   2022-11-20 17:25:06 +0800
+categories: init test
+---
+Sites init test..
+
---
+++ assets/js/_commons/sidebar.js
@@ -0,0 +1,26 @@
+$(function() {
+
+  var sidebarUtil = (function() {
+    const ATTR_DISPLAY = "sidebar-display";
+    var isExpanded = false;
+    var body = $("body");
+
+    return {
+      toggle() {
+        if (isExpanded === false) {
+          body.attr(ATTR_DISPLAY, "");
+        } else {
+          body.removeAttr(ATTR_DISPLAY);
+        }
+
+        isExpanded = !isExpanded;
+      }
+    };
+
+  }());
+
+  $("#sidebar-trigger").click(sidebarUtil.toggle);
+
+  $("#mask").click(sidebarUtil.toggle);
+
+});
